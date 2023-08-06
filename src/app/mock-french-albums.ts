98fca4578be546f0047beadee5c7206df2e3bb43
Tiburce Kouagou <kouagoutiburce@gmai.com>
2023-08-06 19:29:21 +0100

    add a new mock-french-albums with real data

---
+++ src/app/mock-french-albums.ts
@@ -0,0 +1,174 @@
+import { Album } from "./album"
+
+const FRENCH_ALBUMS: Album[] = [
+    {
+        "id": "1",
+        "ref": "Rock",
+        "name": "Élodie Dubois",
+        "title": "Éclaircie",
+        "description": "Laissez-vous emporter par les rythmes envoûtants et les paroles poétiques de l'album 'Éclaircie' par Élodie Dubois. Une expérience musicale unique qui illumine l'esprit et le cœur.\r\n",
+        "duration": 600,
+        "url": "http://placehold.it/32x32",
+        "like": "Beaucoup",
+        "tags": [
+            "rock",
+            "envoûtant",
+            "poétique",
+            "émotionnel"
+        ],
+        "status": "off"
+    },
+    {
+        "id": "2",
+        "ref": "Pop",
+        "name": "Antoine Moreau",
+        "title": "Rêve Étoilé",
+        "description": "Plongez dans l'univers enchanteur de l'album 'Rêve Étoilé' par Antoine Moreau. Des mélodies captivantes et des refrains entraînants vous feront voyager dans un monde de rêves et d'émerveillement.\r\n",
+        "duration": 480,
+        "url": "http://placehold.it/32x32",
+        "like": "Beaucoup",
+        "tags": [
+            "pop",
+            "enchanteur",
+            "mélodique",
+            "rêve"
+        ],
+        "status": "on"
+    },
+    {
+        "id": "3",
+        "ref": "Jazz",
+        "name": "Lucien Dupuis",
+        "title": "Harmonie Nocturne",
+        "description": "Savourez les nuances subtiles et les improvisations magistrales de l'album 'Harmonie Nocturne' par Lucien Dupuis. Ce jazz nocturne éveillera vos sens et illuminera vos nuits.\r\n",
+        "duration": 720,
+        "url": "http://placehold.it/32x32",
+        "like": "Aimer",
+        "tags": [
+            "jazz",
+            "nocturne",
+            "improvisation",
+            "sophistiqué"
+        ],
+        "status": "on"
+    },
+    {
+        "id": "4",
+        "ref": "Chanson",
+        "name": "Manon Martin",
+        "title": "Parfum d'Amour",
+        "description": "tition  Laissez-vous enchanter par les chansons envoûtantes de l'album 'Parfum d'Amour' par Manon Martin. Des mots d'amour tissés dans des mélodies douces qui réchauffent le cœur.\r\n",
+        "duration": 600,
+        "url": "http://placehold.it/32x32",
+        "like": "Super aimer",
+        "tags": [
+            "chanson",
+            "amour",
+            "douces",
+            "émouvant"
+        ],
+        "status": "off"
+    },
+    {
+        "id": "5",
+        "ref": "Rap",
+        "name": "Malik Diop",
+        "title": "Évolution Urbaine",
+        "description": "Explorez l'évolution urbaine à travers les rimes percutantes de l'album 'Évolution Urbaine' par Malik Diop. Un rap engagé qui reflète la vie en milieu urbain.\r\n",
+        "duration": 360,
+        "url": "http://placehold.it/32x32",
+        "like": "Vraiment aimer",
+        "tags": [
+            "rap",
+            "engagé",
+            "urbain",
+            "réaliste"
+        ],
+        "status": "on"
+    },
+    {
+        "id": "6",
+        "ref": "Electro",
+        "name": "Sophie Leroy",
+        "title": "Ondes Électriques",
+        "description": "Plongez dans l'univers électrisant de l'album 'Ondes Électriques' par Sophie Leroy. Des beats électro enivrants qui vous feront danser toute la nuit.\r\n",
+        "duration": 480,
+        "url": "http://placehold.it/32x32",
+        "like": "J'aime beaucoup",
+        "tags": [
+            "electro",
+            "électrisant",
+            "rythmé",
+            "énergétique"
+        ],
+        "status": "off"
+    },
+    {
+        "id": "7",
+        "ref": "Hip-Hop",
+        "name": "Théo Dupont",
+        "title": "Vers d'Acier",
+        "description": "Découvrez la force et la profondeur des paroles dans l'album 'Vers d'Acier' par Théo Dupont. Un hip-hop lyrique qui transmet des messages percutants.\r\n",
+        "duration": 840,
+        "url": "http://placehold.it/32x32",
+        "like": "Beaucoup",
+        "tags": [
+            "hip-hop",
+            "lyrique",
+            "profond",
+            "engagé"
+        ],
+        "status": "on"
+    },
+    {
+        "id": "8",
+        "ref": "Reggae",
+        "name": "Marie Lévesque",
+        "title": "Racines du Soleil",
+        "description": "Vibrez au rythme ensoleillé de l'album 'Racines du Soleil' par Marie Lévesque. Un reggae enivrant qui vous transporte dans les Caraïbes.\r\n",
+        "duration": 720,
+        "url": "http://placehold.it/32x32",
+        "like": "Adorer",
+        "tags": [
+            "reggae",
+            "ensoleillé",
+            "vibrant",
+            "exotique"
+        ],
+        "status": "off"
+    },
+    {
+        "id": "9",
+        "ref": "Blues",
+        "name": "Louis Dubois",
+        "title": "Mélancolie Bleue",
+        "description": "Laissez-vous envoûter par l'âme du blues dans l'album 'Mélancolie Bleue' par Louis Dubois. Des notes de guitare qui expriment la profondeur des émotions.\r\n",
+        "duration": 360,
+        "url": "http://placehold.it/32x32",
+        "like": "Adorer",
+        "tags": [
+            "blues",
+            "mélancolique",
+            "émouvant",
+            "guitare"
+        ],
+        "status": "on"
+    },
+    {
+        "id": "10",
+        "ref": "Classique",
+        "name": "Camille Petit",
+        "title": "Harmonie Symphonique",
+        "description": "Découvrez la majesté de l'harmonie symphonique dans l'album 'Harmonie Symphonique' par Camille Petit. Une expérience musicale qui transcende le temps et l'espace.\r\n",
+        "duration": 480,
+        "url": "http://placehold.it/32x32",
+        "like": "Super aimer",
+        "tags": [
+            "classique",
+            "symphonique",
+            "majestueux",
+            "transcendant"
+        ],
+        "status": "off"
+    }
+]
