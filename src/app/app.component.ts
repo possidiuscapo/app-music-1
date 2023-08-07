ef62fedb0b81a2f4b530628a78caff4a91a6838b
Tiburce Kouagou <kouagoutiburce@gmail.com>
2023-08-07 11:45:41 +0200

    added timer to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-music';
   receivedText: string | undefined;
+  timerObservable!: Observable<string>;
+  // !: opérateur de confiance | opérateur d'affirmation de non nullité,
+  count!: string;
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.timerObservable = interval(1000).pipe(
+      map(num => {
+        const hours = Math.floor(num / 3600);
+        const minutes = Math.floor(num / 60);
+        return `Time: ${this.format(hours)} h ${this.format(minutes - hours * 60)} min ${this.format(num - minutes * 60)} s`;
+      })
+    );
+    this.timerObservable.subscribe(time => {
+      this.count = time;
+    });
+  }
+
+  format(num: number): string {
+    return (num < 10 ? '0' : '') + num;
+  }
 
   parentReceive($event: string) {
     this.receivedText = $event;
